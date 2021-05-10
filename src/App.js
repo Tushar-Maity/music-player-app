7fb532a44447955e0f2fd0f8e6ae32bdc624fdc7
Tushar_Maity <68024552+Tushar-Maity@users.noreply.github.com>
2021-05-10 09:42:40 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+//Import Styles
+import "./styles/app.scss"
+//Adding Components
+import Song from './components/Song';
+import Player from './components/Player';
+import Library from './components/Library';
+//Importing data from util
+import chillHop from './util';
+
+const App = () => {
+  //State
+  const [songs, setSongs] = useState(chillHop());
+  const [currentSong, setCurrentSong] = useState(songs[4]);
+  const [isPlaying, setIsPlaying] = useState(false);
+  return (
+    <div className="App">
+       <Song currentSong={currentSong}/>
+       <Player setIsPlaying={setIsPlaying} isPlaying={isPlaying} currentSong={currentSong}/>
+       <Library songs={songs} setCurrentSong={setCurrentSong}/>
+    </div>
+  );
+}
+
+export default App;
